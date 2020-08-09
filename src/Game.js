a6a4de97ee9cab77d4444d658fce477734af0a5b
Skarn <ceathramh@gmail.com>
2020-08-09 16:03:42 +0200

    Switch syntax for sections

--- src/Game.js
+++ src/Game.js
@@ -4,18 +4,24 @@ import { selectSection, setSection } from "./features/navigation/reducer";
 import { useTranslation } from "react-i18next";
 import Section from "./features/navigation/Section";
 
+const sectionProps = ({ section, t, dispatch }) => {
+  switch (section) {
+    case "ceto":
+      return { text: t("story.meeting.ceto") };
+    default:
+      return {
+        text: t("story.meeting.introduction"),
+        next: () => dispatch(setSection("ceto")),
+      };
+  }
+};
+
 const Game = () => {
   const section = useSelector(selectSection);
   const dispatch = useDispatch();
   const { t } = useTranslation();
 
-  if (section === "ceto") {
-    return <Section text={t("story.meeting.ceto")} />;
-  }
-
-  const next = () => dispatch(setSection("ceto"));
-
-  return <Section text={t("story.meeting.introduction")} next={next} />;
+  return <Section {...sectionProps({ section, t, dispatch })} />;
 };
 
 export default Game;
