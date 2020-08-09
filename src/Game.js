b4cf0bdd5e3a5ff51c2b438c19ee6681104ca3f0
Skarn <ceathramh@gmail.com>
2020-08-09 11:57:36 +0200

    Move Section to own component

--- src/Game.js
+++ src/Game.js
@@ -1,82 +1,8 @@
-import React, { useRef, useImperativeHandle } from "react";
-import sanitizeHtml from "sanitize-html";
+import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import {
-  selectSection,
-  setSection,
-  selectSubSection,
-  nextSubSection,
-} from "./features/navigation/reducer";
+import { selectSection, setSection } from "./features/navigation/reducer";
 import { useTranslation } from "react-i18next";
-import { Typography, Card, Button } from "antd";
-
-const { Paragraph } = Typography;
-
-const SubSection = ({ text }) => {
-  return (
-    <Paragraph>
-      {text.split("\n").map((paragraph, index) => {
-        return (
-          <p
-            key={index.toString()}
-            dangerouslySetInnerHTML={{ __html: sanitizeHtml(paragraph) }}
-          />
-        );
-      })}
-    </Paragraph>
-  );
-};
-
-const ContinueButton = React.forwardRef(({ action }, ref) => {
-  const buttonRef = useRef();
-  useImperativeHandle(ref, () => ({
-    click: () => {
-      buttonRef.current.focus();
-      buttonRef.current.click();
-    },
-  }));
-
-  return (
-    <Button ref={buttonRef} onClick={action}>
-      {">>>"}
-    </Button>
-  );
-});
-
-const Section = ({ text, children, next }) => {
-  const subSectionIndex = useSelector(selectSubSection);
-  const dispatch = useDispatch();
-  const continueRef = useRef();
-
-  const subsections = text.split(/\n{2,}/);
-
-  const show = (list) => {
-    return list.map((text, index) => {
-      return <SubSection key={index.toString()} text={text} />;
-    });
-  };
-
-  if (subSectionIndex >= subsections.length - 1 && !next) {
-    return (
-      <Card>
-        {show(subsections)}
-        {children}
-      </Card>
-    );
-  }
-
-  const action =
-    subSectionIndex >= subsections.length - 1
-      ? next
-      : () => dispatch(nextSubSection());
-
-  return (
-    <Card hoverable onClick={() => continueRef.current.click()}>
-      {show(subsections.filter((_, index) => index <= subSectionIndex))}
-      <ContinueButton ref={continueRef} action={action} />
-    </Card>
-  );
-};
+import Section from "./features/navigation/Section";
 
 const Game = () => {
   const section = useSelector(selectSection);
---
+++ src/features/navigation/Section.js
@@ -0,0 +1,75 @@
+import React, { useRef, useImperativeHandle } from "react";
+import sanitizeHtml from "sanitize-html";
+import { useSelector, useDispatch } from "react-redux";
+import { selectSubSection, nextSubSection } from "./reducer";
+import { Typography, Card, Button } from "antd";
+
+const { Paragraph } = Typography;
+
+const SubSection = ({ text }) => {
+  return (
+    <Paragraph>
+      {text.split("\n").map((paragraph, index) => {
+        return (
+          <p
+            key={index.toString()}
+            dangerouslySetInnerHTML={{ __html: sanitizeHtml(paragraph) }}
+          />
+        );
+      })}
+    </Paragraph>
+  );
+};
+
+const ContinueButton = React.forwardRef(({ action }, ref) => {
+  const buttonRef = useRef();
+  useImperativeHandle(ref, () => ({
+    click: () => {
+      buttonRef.current.focus();
+      buttonRef.current.click();
+    },
+  }));
+
+  return (
+    <Button ref={buttonRef} onClick={action}>
+      {">>>"}
+    </Button>
+  );
+});
+
+const Section = ({ text, children, next }) => {
+  const subSectionIndex = useSelector(selectSubSection);
+  const dispatch = useDispatch();
+  const continueRef = useRef();
+
+  const subsections = text.split(/\n{2,}/);
+
+  const show = (list) => {
+    return list.map((text, index) => {
+      return <SubSection key={index.toString()} text={text} />;
+    });
+  };
+
+  if (subSectionIndex >= subsections.length - 1 && !next) {
+    return (
+      <Card>
+        {show(subsections)}
+        {children}
+      </Card>
+    );
+  }
+
+  const action =
+    subSectionIndex >= subsections.length - 1
+      ? next
+      : () => dispatch(nextSubSection());
+
+  return (
+    <Card hoverable onClick={() => continueRef.current.click()}>
+      {show(subsections.filter((_, index) => index <= subSectionIndex))}
+      <ContinueButton ref={continueRef} action={action} />
+    </Card>
+  );
+};
+
+export default Section;
