fec6ccb4c795b57a62be90e807f78c0daca06fda
Skarn <ceathramh@gmail.com>
2020-08-19 15:18:58 +0200

    Playable meeting

--- src/Game.js
+++ src/Game.js
@@ -3,25 +3,44 @@ import { useSelector, useDispatch } from "react-redux";
 import { selectSection, setSection } from "./features/navigation/reducer";
 import { useTranslation } from "react-i18next";
 import Section from "./features/navigation/Section";
-
-const sectionProps = ({ section, t, dispatch }) => {
-  switch (section) {
-    case "ceto":
-      return { text: t("story.meeting.ceto") };
-    default:
-      return {
-        text: t("story.meeting.introduction"),
-        next: () => dispatch(setSection("ceto")),
-      };
-  }
-};
+import PartySelector from "./features/party/PartySelector";
+import {
+  CETO,
+  ALECTO,
+  CAROLE,
+  KATRINA,
+  TEKELI,
+  CAMILLA,
+  MAHARAL,
+} from "./characters";
 
 const Game = () => {
   const section = useSelector(selectSection);
   const dispatch = useDispatch();
   const { t } = useTranslation();
+  const goTo = (section) => () => dispatch(setSection(section));
 
-  return <Section {...sectionProps({ section, t, dispatch })} />;
+  const meetingOrder = [CETO, ALECTO, CAROLE, KATRINA, TEKELI, CAMILLA];
+  const i = meetingOrder.indexOf(section);
+  if (i > -1) {
+    return (
+      <Section text={t(`story.meeting.${section}`)}>
+        <PartySelector
+          character={section}
+          next={goTo(i >= meetingOrder.length ? MAHARAL : meetingOrder[i + 1])}
+        />
+      </Section>
+    );
+  }
+
+  switch (section) {
+    case MAHARAL:
+      return <Section text={t("story.meeting.maharal")} />;
+    default:
+      return (
+        <Section text={t("story.meeting.introduction")} next={goTo(CETO)} />
+      );
+  }
 };
 
 export default Game;
