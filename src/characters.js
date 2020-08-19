fe6c1bc04e359d7576f5132813592d5efc64d45c
Skarn <ceathramh@gmail.com>
2020-08-19 15:16:26 +0200

    Meta story elements (characters/locations)

---
+++ src/characters.js
@@ -0,0 +1,9 @@
+export const DEJANIRE = "dejanire";
+export const MAHARAL = "maharal";
+export const RASHOMON = "rashomon";
+export const CETO = "ceto";
+export const ALECTO = "alecto";
+export const CAROLE = "carole";
+export const KATRINA = "katrina";
+export const TEKELI = "tekeli";
+export const CAMILLA = "camilla";
